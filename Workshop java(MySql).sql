f6e0bd785412ed97a8099a0dd66ab26e14cd30f2
AyekpamPrithi <ayekpamprithi@gmail.com>
2024-10-23 14:36:19 +0530

    Workshop java(MySql).sql day2 assgnmt

--- Workshop java(MySql).sql
+++ Workshop java(MySql).sql
@@ -1,6 +1,6 @@
  -- TASK Assignment 1,2,3
  -- creating database 
-create database sgvu_dtb;
+create database bootcamp_java;
 use sgvu_dtb;
 
  -- creating tables
@@ -11,6 +11,7 @@ desc student;
  -- inserting records
 insert into student values (1001, "Ayekpam","9362002074", "ayekpamprithi@gmail.com");
 insert into student values (1002, "prithiviraj","9612653135","prithi@gmail.com");
+insert into student values (1003, "Mangang", "123456789","mangang@gmail.com");
 
  -- Fetch the current data
 select * from student;
@@ -18,6 +19,7 @@ insert into student values (1003,"mangang", "9321313132", "here@gmail.com");
 create table users1 (id  int primary key auto_increment, username varchar(100), password varchar(100), balance decimal(10,2));
 insert into users1 (username, password, balance) values ("Ayekpam", "23181072", 10000);
 insert into users1 (username, password, balance) values ("Prithiviraj", "9362002074", 20000);
+insert into users1 (username, password, balance) values ("mangang", "9612653135", 30000);
 
  -- fetch all the record to see the current data
 select * from users1;
@@ -25,16 +27,18 @@ select * from users1;
 --  Check if "Ayekpam" exists before updating
 Select * from users1 where  username = "Ayekpam";
 --  If "Ayekpam" exists, update her balance
-UPDATE users1
-SET balance = 40000
-WHERE username = "Ayekpam";
+update  users1 SET balance = 40000 WHERE id = 1;
+
+
 
 -- Fetch all records again to see the updated data
-SELECT * FROM users1;
+SELECT * from users1;
 
 -- Delete Prithiviraj record from the users table
-DELETE FROM users1
-WHERE username = "Prithiviraj";
+delete from  users1 where id = 1;
+
 
 -- Fetch records again to verify deletion
-SELECT * FROM users1;
+select * from users1;
+
+update users1 Set balance = 14000 where id= 1
